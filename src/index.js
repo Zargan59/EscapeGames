98bd1e1a843497090baec59fb4a3c381ad542e65
Tristan Destoop <tristan.Destoop@gmail.com>
2024-08-31 21:25:04 +0200

    modification du chemin

--- src/index.js
+++ src/index.js
@@ -10,14 +10,20 @@ import HomePage from "./Pages/Home"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={< HomePage />} />
-        <Route path="/Voyage-au-centre-du-corps" element={<Body /> }  />
-      </Routes>
-    </HashRouter>
+      <HomePage />
   </React.StrictMode>
 );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <HashRouter>
+//       <Routes>
+//         <Route path="/" element={< HomePage />} />
+//         <Route path="/Voyage-au-centre-du-corps" element={<Body /> }  />
+//       </Routes>
+//     </HashRouter>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
